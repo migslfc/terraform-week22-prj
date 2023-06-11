890b6d91032bd4ceebb8c05b044b1dbafeaec7b2
MIGUEL CAMACHO <mcamacho0808@gmail.com>
2023-06-11 19:58:21 -0400

    updated variables

--- variables.tf
+++ variables.tf
@@ -1,11 +1,3 @@
-variable "db_username" {
-  description = "The username for the RDS instance"
-}
-
-variable "db_password" {
-  description = "The password for the RDS instance"
-  sensitive   = true
-}
 
 variable "aws_region" {
   description = "region where instance will launch"
