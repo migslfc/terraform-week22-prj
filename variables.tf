634cfff227ef4dde90570e224928219355ceb1bf
MIGUEL CAMACHO <mcamacho0808@gmail.com>
2023-06-11 20:07:07 -0400

    updated variables-1

--- variables.tf
+++ variables.tf
@@ -3,3 +3,11 @@ variable "aws_region" {
   description = "region where instance will launch"
   default     = "us-east-1"
 }
+
+variable "db_username" {
+  description = "Username for the RDS database"
+}
+
+variable "db_password" {
+  description = "Password for the RDS database"
+}
